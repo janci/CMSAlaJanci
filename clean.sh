037da69b32aac3b50c335338f94f0ea6f3742798
Jan Svantner <janci@janci.net>
2012-05-13 03:59:20 +0200

    repair error with with in plugin News, clear table with git checkout in clean.sh

--- clean.sh
+++ clean.sh
@@ -4,11 +4,5 @@ rm -rf temp/cache/_Nette.*
 
 database=app/database/data.sq3
 
-sqlite3 $database  'DROP TABLE IF EXISTS `article`'
-
-sqlite3 $database  'DROP TABLE IF EXISTS `page`'
-sqlite3 $database  'DROP TABLE IF EXISTS `new`'
-sqlite3 $database  'DROP TABLE IF EXISTS `photo`'
-sqlite3 $database  'DROP TABLE IF EXISTS `album`'
-sqlite3 $database  'DROP TABLE IF EXISTS `document`'
-sqlite3 $database  'DROP TABLE IF EXISTS `seo`'
+git checkout $database
+chmod 777 $database
